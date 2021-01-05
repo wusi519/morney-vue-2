d86d14cd8a242431ff4c7c472b0a79e40beb8150
FredXu <327742525@qq.com>
2021-01-05 21:27:24 +0800

    添加全局样式和全局变量

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
-  <div>
+  <div id="app">
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+  @import "~@/assets/style/helper.scss";
+  @import "~@/assets/style/reset.scss";
+
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #333;
+    font-family: $font-hei;
+    font-size: 16px;
+    line-height: 1.5;
   }
 
+  #nav {
+    padding:30px;
+  a{
+    font-weight:bold;
+    color:#2C3E50;
+    &.router-link-exact-active{
+      color:#42B923;
+    }
+  }
+  }
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,31 +1,50 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">
+  <nav>
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
 
-    |
-    <router-link to="/labels">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       标签
     </router-link>
-    |
-    <router-link to="/statistics">
+
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
+  import Icon from '@/components/Icon.vue';
+
+  const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+  try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
   export default {
     name: 'Nav'
   };
+
 </script>
 
-<style lang="scss">
-  .nav {
-    border: 1px solid red;
+<style lang="scss" scoped>
+  @import "~@/assets/style/helper.scss";
+  nav {
+     display: flex;
+    flex-direction: row;
+    font-size: 12px;
+
+    > .item {
+      padding: 2px 0;
+      width: 33.33%;
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+    }
+
+    > .item.selected {
+      color: $color-highlight;
+    }
   }
 </style>
